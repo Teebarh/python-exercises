653f25a51c0ee4703a60d22963633a1ad06a3297
Toyibat Adele <112214060+Teebarh@users.noreply.github.com>
2023-12-21 10:11:34 +0100

    Add files via upload

---
+++ Variables_and_simple_data_types/number_eight.py
@@ -0,0 +1,6 @@
+#Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print() calls to see the results.
+
+print(4+4)
+print(18-10)
+print(2*4)
+print(16/2)
