d96869f667ba3359fe36d6704acae8537e2fe7e1
Toyibat Adele <112214060+Teebarh@users.noreply.github.com>
2023-12-21 10:13:38 +0100

    Add files via upload

---
+++ Variables_and_simple_data_types/favourite_number.py
@@ -0,0 +1,4 @@
+# Use a variable to represent your favorite number. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+favourite_number = 5
+print(f"My favourite number is {favourite_number}!")
